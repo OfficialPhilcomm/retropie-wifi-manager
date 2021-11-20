c856295ca61d38b7c60a71e738c0908f0c02c686
Philipp Schlesinger <info@philcomm.dev>
2021-11-20 19:22:18 +0100

    Improve scriptmodule

--- advanced-wifi.sh
+++ advanced-wifi.sh
@@ -13,7 +13,7 @@ rp_module_id="advanced-wifi"
 rp_module_desc="Advanced wifi manager with OSK"
 rp_module_help="This is a wifi manager terminal app with an on screen keyboard, for when no keyboard is in reach!"
 rp_module_section="exp"
-rp_module_flags="rpi nobin"
+rp_module_flags="rpi noinstclean nobin"
 rp_module_repo="git https://github.com/OfficialPhilcomm/retropie-wifi-manager.git master"
 
 function depends_advanced-wifi() {
@@ -30,4 +30,33 @@ function install_advanced-wifi() {
   chmod -R 755 "$md_inst"
 
   sudo gem install curses require_all
+}
+
+function gui_advanced-wifi() {
+  local cmd=()
+  local options=(
+    1 "Add advanced-wifi to RetroPie Menu"
+    2 "Remove advanced-wifi from RetroPie Menu"
+  )
+  local choice
+  local error_msg
+  
+  while true; do
+    cmd=(dialog --backtitle "$__backtitle" --menu "What do you wanna do?" 22 86 16)
+    choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+    
+    if [[ -n "$choice" ]]; then
+      case "$choice" in
+        1)
+          cp "$md_inst/wifi2.sh" /home/pi/RetroPie/retropiemenu/
+          ;;
+
+        2)
+          rm /home/pi/RetroPie/retropiemenu/wifi2.sh
+          ;;
+      esac
+    else
+      break
+    fi
+  done
 }
--- wifi2.sh
+++ wifi2.sh
@@ -1,1 +1,1 @@
-sudo ruby /opt/dev_philcomm/wifi2/wifi2.rb
+sudo ruby /opt/retropie/supplementary/advanced-wifi/wifi2/wifi2.rb
