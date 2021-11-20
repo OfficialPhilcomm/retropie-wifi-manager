cc50f505c0a0c857716baa1ef399c53a9e938ee0
Philipp Schlesinger <info@philcomm.dev>
2021-11-20 21:40:11 +0100

    Adjust styling to be more in line with other retropie tools

--- wifi2/wifi2.rb
+++ wifi2/wifi2.rb
@@ -21,8 +21,24 @@ begin
   init_pair(COLOR_RED, 1, 7)
   init_pair(COLOR_GREEN, 2, 7)
   init_pair(COLOR_BLUE, 6, 7)
+  init_pair(5, 7, 4)
 
-  window = Curses::Window.new(0, 0, 1, 2)
+  max_x = Curses.cols
+  max_y = Curses.lines
+  padding_x = [0, max_x, max_x - 50].sort[1]
+  padding_y = [0, max_y, max_y - 20].sort[1]
+
+  background_window = Curses::Window.new(0, 0, 0, 0)
+  background_window.bkgdset(color_pair(5))
+  background_window.clear
+  background_window.refresh
+
+  window = Curses::Window.new(
+    max_y - padding_y,
+    max_x - padding_x,
+    padding_y / 2,
+    padding_x / 2
+  )
   window.bkgdset(color_pair(DEFAULT_COLOR))
   window.keypad true
 
